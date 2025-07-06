6de962cbeab571bffbd77ec9701142452a33def6
Robert <nowak021@gmail.com>
2025-07-06 22:25:25 +0200

    add zip with password

--- src/command_builder.rs
+++ src/command_builder.rs
@@ -11,19 +11,13 @@ pub fn unpack() -> Result<String, Box<dyn Error>> {
 }
 
 pub fn zip() -> Result<String, Box<dyn Error>> {
-    let file_metadata = input_handler::read_path_to_file_or_directory()?;
-    let destination_archive = get_unique_archive_name();
-    let path = file_metadata.path;
-    let file_to_zip = match file_metadata.is_directory {
-        true => format!("{path}/*"),
-        false => path,
-    };
-    let cmd = format!("zip -r {destination_archive} {file_to_zip}");
-    Ok(cmd)
+    let cmd = get_zip_command()?;
+    Ok(format!("zip -r {cmd}"))
 }
 
 pub fn zip_with_password() -> Result<String, Box<dyn Error>> {
-    todo!()
+    let cmd = get_zip_command()?;
+    Ok(format!("zip -er {cmd}"))
 }
 
 pub fn tar() -> Result<String, Box<dyn Error>> {
@@ -34,4 +28,15 @@ pub fn tar() -> Result<String, Box<dyn Error>> {
 fn get_unique_archive_name() -> String {
     let uuid = Uuid::new_v4();
     format!("archive_{uuid}.zip")
+}
+
+fn get_zip_command() -> Result<String, Box<dyn Error>> {
+    let file_metadata = input_handler::read_path_to_file_or_directory()?;
+    let destination_archive = get_unique_archive_name();
+    let path = file_metadata.path;
+    let file_to_zip = match file_metadata.is_directory {
+        true => format!("{path}/*"),
+        false => path,
+    };
+    Ok(format!("{destination_archive} {file_to_zip}"))
 }
