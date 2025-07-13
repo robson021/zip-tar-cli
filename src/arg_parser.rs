e6ed720a39e579c8974f46673b2515d71c9878c4
Robert <nowak021@gmail.com>
2025-07-14 00:02:25 +0200

    refactor

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -7,13 +7,8 @@ use std::error::Error;
 const EXPECTED_NUMER_OF_ARGS: usize = 3;
 
 pub fn parse_and_run(cmd_args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    let number_of_args = cmd_args.len();
-    if number_of_args != EXPECTED_NUMER_OF_ARGS {
-        let msg = format!(
-            "Invalid number of arguments. Expected {EXPECTED_NUMER_OF_ARGS}, but was {number_of_args}.",
-        );
-        return Err(OperationError::FailedToRunCommand(msg).into());
-    }
+    validate_number_of_args(cmd_args.len())?;
+
     let action = &cmd_args[1];
     let path = &cmd_args[2];
 
@@ -25,10 +20,21 @@ pub fn parse_and_run(cmd_args: Vec<String>) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[inline(always)]
+fn validate_number_of_args(number_of_args: usize) -> Result<(), Box<dyn Error>> {
+    if number_of_args != EXPECTED_NUMER_OF_ARGS {
+        let expected = EXPECTED_NUMER_OF_ARGS - 1;
+        let actual = number_of_args - 1;
+        let msg = format!("Invalid number of arguments. Expected {expected}, but was {actual}.",);
+        return Err(OperationError::FailedToRunCommand(msg).into());
+    }
+    Ok(())
+}
+
 #[inline]
 fn parse_cmd(action: &str, path: &str) -> Result<String, Box<dyn Error>> {
     let cmd = match action {
-        "-u" | "--unpack" | "-d" | "--decompress" => unpack_path(path)?,
+        "-x" | "-u" | "--unpack" | "-d" | "--decompress" => unpack_path(path)?,
         "-z" | "--zip" => zip_path(path, false)?,
         "-ze" | "-ez" | "--zip_encrypt" => zip_path(path, true)?,
         "-t" | "--tar" => tar_path(path)?,
@@ -49,7 +55,7 @@ mod tests {
 
     #[test]
     fn parse_decompress() {
-        for arg in ["-u", "--unpack", "-d", "--decompress"] {
+        for arg in ["-u", "--unpack", "-d", "--decompress", "-x"] {
             let cmd = parse_cmd(arg, TEST_FILES).unwrap();
             assert_eq!(cmd, format!("tar -xvf {TEST_FILES}"));
         }
